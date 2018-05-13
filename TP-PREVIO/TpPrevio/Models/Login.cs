81c6476bbfe4a0b8211179a20ed87096a3eb285b
lorenamartin <martin.lorenasofia@gmail.com>
2018-05-13 20:51:53 -0300

    subiendo codigo tp previo

---
+++ TP-PREVIO/TpPrevio/Models/Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TpPrevio.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Ingrese username")]
+        public string UserName { get; set; }
+        
+        [Required(ErrorMessage ="Ingrese password")]
+        //[Required]   
+        [StringLength(100,ErrorMessage ="Password debe ser al menos de 6 caracteres",MinimumLength =6)]
+        //[DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
